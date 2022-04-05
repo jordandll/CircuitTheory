419b9605023229ca7be2b10f82bcbf5aaa415163
Jordan Driscoll <drisjor@protonmail.com>
2022-04-05 16:10:35 -0400

    Added the InductiveBandPassFilter class to the filters.py module.

--- AlternatingCurrent/filters.py
+++ AlternatingCurrent/filters.py
@@ -16,14 +16,14 @@ class CapacitiveBandPassFilter:
         self.E = E; self.magE = mag(E)
         self.c1 = self.capacitor1 = c1; self.C_1 = c1.capacitance
         self.c2 = self.capacitor2 = c2; self.C_2 = c2.capacitance
-        self.r1 = self.resistor1 = r1; self.R_1 = r1.resistance
+        self.r1 = self.resistor1 = r1; self.R_1 = r1.resistance; self.Z_1 = r1.impedance
         self.load = load; self.R_L = load.resistance
 
     def _Z_a(self, f):
-        return complex(0, -self.capacitor1.reactance(f))
+        return self.capacitor1.impedance(f)
 
     def _Z_b(self, f):
-        return complex(self.load.resistance, -self.capacitor2.reactance(f))
+        return self.load.impedance + self.capacitor2.impedance(f)
 
     def _D(self, f):
         return 1/self._Z_a(f) + 1/self._Z_b(f)
@@ -33,7 +33,7 @@ class CapacitiveBandPassFilter:
         return 1/self._D(f)
 
     def _D_T(self, f):
-        return self.resistor1.impedance + self.Z_2(f)
+        return self.Z_1 + self.Z_2(f)
 
     _Z_T = _D_T
 
@@ -60,3 +60,41 @@ class CapacitiveBandPassFilter:
     def load_voltage_mag(self, f):
         """The amplitude (or magnitude) of the output voltage signal."""
         return self.R_L*mag(self.I_b(f))
+
+
+class InductiveBandPassFilter:
+
+
+    def __init__(self, E: complex, r1: Resistor, l1: Inductor, l2: Inductor, load: Resistor):
+        self.E = E; self.magE = mag(E)
+        self.r1 = self.resistor1 = r1; self.Z_1 = r1.impedance
+        self.l1 = self.inductor1 = l1
+        self.l2 = self.inductor2 = l2
+        self.load = load
+
+    def _Z_a(self, f):
+        return self.inductor1.impedance(f)
+
+    def _Z_b(self, f):
+        return self.load.impedance + self.inductor2.impedance(f)
+
+    def _D(self, f):
+        return 1/self._Z_a(f) + 1/self._Z_b(f)
+
+    def Z_2(self, f):
+        return 1/self._D(f)
+
+    def Z_T(self, f):
+        return self.Z_1 + self.Z_2(f)
+
+    def I(self, f):
+        return self.E/self.Z_T(f)
+
+    def I_b(self, f):
+        return (self._Z_a(f)*self.I(f))/(self._Z_a(f) + self._Z_b(f))
+
+    def load_voltage(self, f):
+        return self.load.impedance * self.I_b(f)
+
+    def load_voltage_mag(self, f):
+        return self.load.resistance * mag(self.I_b(f))
